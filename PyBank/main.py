c322f4bd4feae6c4b9efc37d2b258b301fadc5f1
Soumya Patil <getme1104@gmail.com>
2020-03-21 12:47:52 -0700

    changed the CSV file reading method, Average of Profit losses accurately achieved

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,3 @@
-# First we'll import the os module 
 # This will allow us to create file paths across operating systems
 import os
 
@@ -8,43 +7,54 @@ import csv
 input_path = os.path.join('..', 'PyBank', 'budget_data.csv') #specify the file path to read from
 output_path = os.path.join('..','PyBank','Text_Output.txt')  # specify the file to write to
 
-# Variables to track
 total_months= 0 #to count total_months
 prev_revenue = 0 # to calculate change in revenue by month
 monthOfChange = [] #list to keep months showing change
-revenueChangeList = [] #list to track the change of revenue 
-greatestIncrease = ["",0] # create a List with place holders for date of change and amount
-greatestDecrease = ["",99999999999999] # # create a List with place holders for date of change and amount
+revenueChangeList = [] #list to track the change of revenue ,so as to sum up later
+revenueChange = 0 
 total_ProfitLosses = 0 
 
-# Since the CSV file can be comprehended as KEY-VALUE (names of column:Values of Data) pairs of a dictionary.
-# the csv.DictReader() returns a dictionary for each row read of the CSV file . 
-with open(input_path) as csvfile:
-    # create an object of the csv_DictReader() which can be iterated using a for loop
-    csvreader = csv.DictReader(csvfile)
+# Open the CSV file and create an object of it for iteration
+with open(input_path, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile , delimiter = ",")
 
+    #Reading the header row
+    csv_header = next(csvreader)
+
+    #Reading the first row to compare changes in relative to rest 
+    first_row = next(csvreader)
+    total_months +=1
+    total_ProfitLosses += int(first_row[1])
+    prev_revenue = int(first_row[1])
+
+    # for second row and there on loop it till the end
     for row in csvreader:
-        #count total months and total revenue
-        total_months = total_months + 1
-        total_ProfitLosses = total_ProfitLosses + int(row["Profit/Losses"])
+        #keep the dates updated
+        monthOfChange.append(row[0])
 
-        #track the change in revenue
-        revenueChange = int(row["Profit/Losses"])- prev_revenue
-        prev_revenue = int(row["Profit/Losses"])
-        revenueChangeList = revenueChangeList + [revenueChange]
-        monthOfChange = monthOfChange + [row["Date"]]
-        # Calculate the AverageChange in Profit/Losses for the entire period
-        Avg_Change = sum(revenueChangeList) / len(revenueChangeList)  #changes/number of times its changed 
-        
-        #Calculate the Greatest Increase in Profit
-        if(revenueChange > greatestIncrease[1]):
-            greatestIncrease[0] = row["Date"]
-            greatestIncrease[1] = revenueChange
+        #Calculate the revenue Change,then add it to the list of revenueChanges
+        revenueChange = int(row[1]) - prev_revenue
+        revenueChangeList.append(revenueChange)
+        prev_revenue = int(row[1])
 
-        #Calculate the Greatest Decrease in Losses
-        if(revenueChange < greatestDecrease[1]):
-            greatestDecrease[0] = row["Date"]
-            greatestDecrease[1] = revenueChange
+        #Total number of months
+        total_months += 1
+         
+        #Total net amount of Profit/Losses over entire period
+        total_ProfitLosses = total_ProfitLosses + int(row[1])
+       
+    # Greatest Increase in Profits
+    greatestIncrease = max(revenueChangeList)
+    greatest_index = revenueChangeList.index(greatestIncrease)
+    greatest_date = monthOfChange[greatest_index]
+
+    # Greatest Decrease in Losses 
+    greatestDecrease = min(revenueChangeList)
+    lowest_index = revenueChangeList.index(greatestDecrease)
+    lowest_date = monthOfChange[lowest_index]
+
+    # Average Change in Profit/Losses over the entire period
+    Avg_Change = sum(revenueChangeList)/len(revenueChangeList)
 
 #Format the Output for Display
 output = (
@@ -53,8 +63,8 @@ output = (
     f"Total Months: {total_months}\n"
     f"Total Net amount of Profit/Losses Over Entire Period:   $ {total_ProfitLosses}\n"
     f"Average of changes in Profit/Losses Over Entire Period: $ {str(round(Avg_Change,2))}\n"
-    f"Greatest Increase in Profits: {greatestIncrease[0]}  ,  $ {greatestIncrease[1]}\n"
-    f"Greatest Decrease in Losses : {greatestDecrease[0]}  ,  $ {greatestDecrease[1]}\n")
+    f"Greatest Increase in Profits: {greatest_date}  ,  $ {str(greatestIncrease)}\n"
+    f"Greatest Decrease in Losses : {lowest_date}  ,  $ {str(greatestDecrease)}\n")
  
  #Print OutPut to terminal
 print(output)
